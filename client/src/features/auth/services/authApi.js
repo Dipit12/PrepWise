6f2b06c23062b484dd1d4c260335de1782aadea1
Dipit12 <dipitmadan@gmail.com>
2026-04-13 03:29:19 +0530

    fixed the baseURL

--- client/src/features/auth/services/authApi.js
+++ client/src/features/auth/services/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://prepwise-1-yc3u.onrender.com",
+  baseURL: "https://prepwise-2.onrender.com",
   withCredentials: true,
 });
 
