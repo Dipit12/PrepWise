fe908295fdf316bb7bc00e5f59bd97d6a662d5a3
Dipit12 <dipitmadan@gmail.com>
2026-04-13 04:45:29 +0530

    updated the base url

--- client/src/features/interview/services/interviewApi.js
+++ client/src/features/interview/services/interviewApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://prepwise-7.onrender.com";
+const API_BASE_URL = "https://prepwise-8.onrender.com";
 // For local development, change to: "http://localhost:3001"
 
 const api = axios.create({
